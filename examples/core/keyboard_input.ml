2e17e9dac8820a1ef4dfe178264f78be74751896
Mateus Cruz <mateuscolvr@gmail.com>
2023-09-15 01:28:39 -0300

    keyboard input

---
+++ examples/core/keyboard_input.ml
@@ -0,0 +1,82 @@
+open Raylib
+
+let incr_x ball =
+  match ball with
+  | Vector2 info -> info.x <- info.x +. 2.0
+;;
+
+let incr_y ball =
+  match ball with
+  | Vector2 info -> info.y <- info.y +. 2.0
+;;
+
+let decr_y ball =
+  match ball with
+  | Vector2 info -> info.y <- info.y -. 2.0
+;;
+
+let decr_x ball =
+  match ball with
+  | Vector2 info -> info.x <- info.x -. 2.0
+;;
+
+let move_right () = is_key_down Right
+let move_up () = is_key_down Up
+let move_left () = is_key_down Left
+let move_down () = is_key_down Down
+
+let move ball () =
+  match move_left (), move_up (), move_right (), move_down () with
+  | false, false, false, true -> incr_y ball
+  | false, false, true, false -> incr_x ball
+  | false, false, true, true ->
+    incr_x ball;
+    incr_y ball
+  | false, true, false, false -> decr_y ball
+  | false, true, true, true ->
+    decr_y ball;
+    incr_x ball
+  | false, true, true, false ->
+    decr_y ball;
+    incr_x ball
+  | true, false, false, false -> decr_x ball
+  | true, false, false, true ->
+    decr_x ball;
+    incr_y ball
+  | true, false, true, true -> incr_y ball
+  | true, true, false, false ->
+    decr_x ball;
+    decr_y ball
+  | true, true, false, true -> decr_x ball
+  | true, true, true, false -> decr_y ball
+  | false, false, false, false
+  | false, true, false, true
+  | true, true, true, true
+  | true, false, true, false -> ()
+;;
+
+let rec loop ball () =
+  if window_should_close ()
+  then close_window ()
+  else (
+    let () = move ball () in
+    let () = begin_drawing () in
+    let () = clear_background Color.white in
+    let () = draw_text "move the ball with arrow keys" 10 10 20 Color.lightgray in
+    let () = draw_circle_v ball 50.0 Color.maroon in
+    let () = end_drawing () in
+    loop ball ())
+;;
+
+let () =
+  let screen_width = 800 in
+  let screen_height = 450 in
+  let title = "raylib [core] example - keyboard input" in
+  let ball =
+    Vector2
+      { x = float_of_int screen_width /. 2.0; y = float_of_int screen_height /. 2.0 }
+  in
+  let () = init_window screen_width screen_height title in
+  let () = set_target_fps 60 in
+  loop ball ()
+;;
--- src/lib/rl.c
+++ src/lib/rl.c
@@ -91,3 +91,255 @@ CAMLprim value caml_set_trace_log_level(value level)
     SetTraceLogLevel(log_level_of_caml(level));
     CAMLreturn(Val_unit);
 }
+
+int key_of_ocaml(value key)
+{
+    switch (Int_val(key)) {
+        case 0:
+            return KEY_APOSTROPHE;
+        case 1:
+            return KEY_COMMA;
+        case 2:
+            return KEY_MINUS;
+        case 3:
+            return KEY_PERIOD;
+        case 4:
+            return KEY_SLASH;
+        case 5:
+            return KEY_ZERO;
+        case 6:
+            return KEY_ONE;
+        case 7:
+            return KEY_TWO;
+        case 8:
+            return KEY_THREE;
+        case 9:
+            return KEY_FOUR;
+        case 10:
+            return KEY_FIVE;
+        case 11:
+            return KEY_SIX;
+        case 12:
+            return KEY_SEVEN;
+        case 13:
+            return KEY_EIGHT;
+        case 14:
+            return KEY_NINE;
+        case 15:
+            return KEY_SEMICOLON;
+        case 16:
+            return KEY_EQUAL;
+        case 17:
+            return KEY_A;
+        case 18:
+            return KEY_B;
+        case 19:
+            return KEY_C;
+        case 20:
+            return KEY_D;
+        case 21:
+            return KEY_E;
+        case 22:
+            return KEY_F;
+        case 23:
+            return KEY_G;
+        case 24:
+            return KEY_H;
+        case 25:
+            return KEY_I;
+        case 26:
+            return KEY_J;
+        case 27:
+            return KEY_K;
+        case 28:
+            return KEY_L;
+        case 29:
+            return KEY_M;
+        case 30:
+            return KEY_N;
+        case 31:
+            return KEY_O;
+        case 32:
+            return KEY_P;
+        case 33:
+            return KEY_Q;
+        case 34:
+            return KEY_R;
+        case 35:
+            return KEY_S;
+        case 36:
+            return KEY_T;
+        case 37:
+            return KEY_U;
+        case 38:
+            return KEY_V;
+        case 39:
+            return KEY_W;
+        case 40:
+            return KEY_X;
+        case 41:
+            return KEY_Y;
+        case 42:
+            return KEY_Z;
+        case 43:
+            return KEY_LEFT_BRACKET;
+        case 44:
+            return KEY_BACKSLASH;
+        case 45:
+            return KEY_RIGHT_BRACKET;
+        case 46:
+            return KEY_GRAVE;
+        case 47:
+            return KEY_SPACE;
+        case 48:
+            return KEY_ESCAPE;
+        case 49:
+            return KEY_ENTER;
+        case 50:
+            return KEY_TAB;
+        case 51:
+            return KEY_BACKSPACE;
+        case 52:
+            return KEY_INSERT;
+        case 53:
+            return KEY_DELETE;
+        case 54:
+            return KEY_RIGHT;
+        case 55:
+            return KEY_LEFT;
+        case 56:
+            return KEY_DOWN;
+        case 57:
+           return KEY_UP;
+        case 58:
+           return KEY_PAGE_UP;
+        case 59:
+           return KEY_PAGE_DOWN;
+        case 60:
+           return KEY_HOME;
+        case 61:
+           return KEY_END;
+        case 62:
+           return KEY_CAPS_LOCK;
+        case 63:
+           return KEY_SCROLL_LOCK;
+        case 64:
+           return KEY_NUM_LOCK;
+        case 65:
+           return KEY_PRINT_SCREEN;
+        case 66:
+           return KEY_PAUSE;
+        case 67:
+           return KEY_F1;
+        case 68:
+           return KEY_F2;
+        case 69:
+           return KEY_F3;
+        case 70:
+           return KEY_F4;
+        case 71:
+           return KEY_F5;
+        case 72:
+           return KEY_F6;
+        case 73:
+           return KEY_F7;
+        case 74:
+           return KEY_F8;
+        case 75:
+           return KEY_F9;
+        case 76:
+           return KEY_F10;
+        case 77:
+           return KEY_F11;
+        case 78:
+           return KEY_F12;
+        case 79:
+           return KEY_LEFT_SHIFT;
+        case 80:
+           return KEY_LEFT_CONTROL;
+        case 81:
+           return KEY_LEFT_ALT;
+        case 82:
+           return KEY_LEFT_SUPER;
+        case 83:
+           return KEY_RIGHT_SHIFT;
+        case 84:
+           return KEY_RIGHT_CONTROL;
+        case 85:
+           return KEY_RIGHT_ALT;
+        case 86:
+           return KEY_RIGHT_SUPER;
+        case 87:
+           return KEY_KB_MENU;
+        case 88:
+           return KEY_KP_0;
+        case 89:
+           return KEY_KP_1;
+        case 90:
+           return KEY_KP_2;
+        case 91:
+           return KEY_KP_3;
+        case 92:
+           return KEY_KP_4;
+        case 93:
+           return KEY_KP_5;
+        case 94:
+           return KEY_KP_6;
+        case 95:
+           return KEY_KP_7;
+        case 96:
+           return KEY_KP_8;
+        case 97:
+           return KEY_KP_9;
+        case 98:
+           return KEY_KP_DECIMAL;
+        case 99:
+           return KEY_KP_DIVIDE;
+        case 100:
+           return KEY_KP_MULTIPLY;
+        case 101:
+           return KEY_KP_SUBTRACT;
+        case 102:
+           return KEY_KP_ADD;
+        case 103:
+           return KEY_KP_ENTER;
+        case 104:
+           return KEY_KP_EQUAL;
+        case 105:
+           return KEY_BACK;
+        case 106:
+           return KEY_MENU;
+        case 107:
+           return KEY_VOLUME_UP;
+        case 108:
+           return KEY_VOLUME_DOWN;
+    }
+}
+
+Vector2 vector2_of_caml(value vector)
+{
+    float x = Double_val(Field(vector, 0));
+    float y = Double_val(Field(vector, 1));
+    return (Vector2) { x, y };
+}
+
+CAMLprim value caml_draw_circle_v(value center, value radius, value color)
+{
+    CAMLparam3(center, radius, color);
+    Vector2 vec = vector2_of_caml(center);
+    double rad = Double_val(radius);
+    Color cor = color_of_caml(color);
+    DrawCircleV(vec, rad, cor);
+    CAMLreturn(Val_unit);
+}
+
+CAMLprim value caml_is_key_down(value key)
+{
+    CAMLparam1(key);
+    if (IsKeyDown(key_of_ocaml(key)))
+        {
+            CAMLreturn(Val_true);
+        } else {
+        CAMLreturn(Val_false);
+    }
+}
